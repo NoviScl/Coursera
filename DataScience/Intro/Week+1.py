bd76023729aad856fe0a209126d34280f2c1c548
NoviScl <sichenglei1125@gmail.com>
2018-04-29 15:47:29 +0800

    pythonSimple

--- DataScience/Intro/Week+1.py
+++ DataScience/Intro/Week+1.py
@@ -413,5 +413,13 @@ for i, row in enumerate(test):
 test2 = test**2
 test2
 
+print (test, test2)
 for i, j in zip(test, test2):
     print(i,'+',j,'=',i+j)
+
+#use of zip
+a = [1,2,3]
+b = [4,5,6]
+#zip(a,b) ==> [(1,4), (2,5), (3,6)]
+for i in zip(a,b):
+    print (i)
